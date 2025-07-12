97543137cdb6d2f767541da69c91f1960afc941e
drddro <drddro@gmail.com>
2025-07-12 21:54:03 +0200

    added routing to main-menu-component when setup is done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {Setup} from './pages/setup/setup';
+import {MainMenuComponent} from './pages/main-menu-component/main-menu-component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Setup],
+  imports: [Setup, MainMenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
