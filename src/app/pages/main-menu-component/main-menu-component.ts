8c57305ff9d7a88f55bbe9b6426b34909de62dcc
drddro <drddro@gmail.com>
2025-07-12 23:54:05 +0200

    added question cards

--- src/app/pages/main-menu-component/main-menu-component.ts
+++ src/app/pages/main-menu-component/main-menu-component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {QuestionService} from '../../services/question-service';
 import {UserService} from '../../services/user-service';
+import {QuestionCardComponent} from '../../utils/question-card-component/question-card-component';
 
 @Component({
   selector: 'app-main-menu-component',
-  imports: [],
+  imports: [
+    QuestionCardComponent
+  ],
   templateUrl: './main-menu-component.html',
   styleUrl: './main-menu-component.css'
 })
---
+++ src/app/utils/question-card-component/question-card-component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Question } from '../../models/Question';
+
+@Component({
+  selector: 'app-question-card-component',
+  imports: [],
+  templateUrl: './question-card-component.html',
+  styleUrl: './question-card-component.css'
+})
+export class QuestionCardComponent{
+
+  @Input() question!: Question;
+
+  constructor() {
+  }
+
+  routeToQuestion() {
+
+  }
+}
