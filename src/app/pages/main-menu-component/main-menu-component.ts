776c487c11101fedfa03de684ece09f6d1f60ce0
drddro <drddro@gmail.com>
2025-07-12 21:54:51 +0200

    added main menu

---
+++ src/app/pages/main-menu-component/main-menu-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu-component',
+  imports: [],
+  templateUrl: './main-menu-component.html',
+  styleUrl: './main-menu-component.css'
+})
+export class MainMenuComponent {
+
+}
